af14e31b0910334a7d1e82b630a3806468133051
Gokul Nair <gokul.getpowerplay.in>
2022-12-15 07:13:44 +0530

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by Gokul Nair on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
